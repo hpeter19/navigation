a092361de882be237f06f0a6f82779dde937fb4c
hpeter19 <njoshpitar@gmail.com>
2025-04-13 16:16:53 +0300

    animating

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const navSlide =() =>{
       //toggle nav
      burger.addEventListener('click', () =>{
          nav.classList.toggle('nav-active');
+
+         navLinks.forEach((link,index) =>{
+            if(link.style.animation){
+                link.style.animation=``
+            }
+            else{
+                link.style.animation=`navLinkFade 0.5s ease forwards ${index / 7 + 1.3}s`;
+            }
+         });
      });
      //animate links
-     navLinks.forEach((link,index) =>{
-        link.style.animation=`navLinkFade 0.5s ease forwards ${index / 7}s`;
-        console.log(index / 7);
-     });
 }
 
 navSlide();
